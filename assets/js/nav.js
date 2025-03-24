273dd8f68ab7354e02e6fafe582e738392a4361c
DJAKALET Jean Jacques Yao <jeanjackdjakale@gmail.com>
2025-03-24 23:20:44 +0000

    mise à jour

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,1 +1,13 @@
-docu;
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".fa-bars");
+  const closeIcon = document.querySelector(".nav-element2 .fa-xmark");
+  const navElement = document.querySelector(".nav-element2");
+
+  menuIcon.addEventListener("click", function () {
+    navElement.classList.add("show");
+  });
+
+  closeIcon.addEventListener("click", function () {
+    navElement.classList.remove("show");
+  });
+});
